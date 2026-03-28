a0f6a8f233a4d19214cd418c42e38a8e2cdb656a
AntonioJr <afs.junior21@gmail.com>
2026-03-28 14:19:49 -0300

    feat: update cors for vercel frontend

--- src/main/java/afsj/efm/auth/security/config/CorsConfig.java
+++ src/main/java/afsj/efm/auth/security/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
    CorsConfigurationSource corsConfigurationSource() {
       CorsConfiguration configuration = new CorsConfiguration();
 
-      configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+      configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://farm-easy-flow.vercel.app"));
       configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
       configuration.setAllowedHeaders(List.of("*"));
 
